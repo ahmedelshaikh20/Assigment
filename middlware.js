a26409ddcf1bb86108d73a1ec37f00c4e57b231a
Ahmed Salah <ahmedelshaikh51@gmail.com>
2021-12-14 04:28:47 +0200

    Fixing some errors

--- middlware.js
+++ middlware.js
@@ -2,17 +2,11 @@ const Joi = require("joi");
 const schemas =require('./validation')
 const middleware = (schema) => {
   return (req, res, next) => {
-    if (req.params.type === "Fedex"){
-      schema = schemas.FedexBody
-    }
-    else if (req.params.type ==="Ups"){
-      schema = schemas.UpsBody
-    }else {
-     return res.status(422).json({ error :"Type is not valid"});
-    }
+    
+    
     const { error } = schema.validate(req.body);
     //const { error } = Joi.validate(req.body, schema);
-    console.log(error)
+    console.log(req.body)
     const valid = error == null;
     if (valid) {
       next();
@@ -20,7 +14,7 @@ const middleware = (schema) => {
       const { details } = error;
       const message = details.map((i) => i.message).join(",");
       console.log("error", message);
-      res.status(422).json({ error: message });
+      res.status(422).json({ "Status" :"Faild to create shipment",error: message });
     }
   };
 };
