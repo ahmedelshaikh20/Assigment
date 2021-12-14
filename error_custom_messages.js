69e179ac9e590accd3059088beccf5611d9f8e49
Ahmed Salah <ahmedelshaikh51@gmail.com>
2021-12-14 04:31:12 +0200

    fixerrors

--- error_custom_messages.js
+++ error_custom_messages.js
@@ -4,10 +4,11 @@ const ErrorMessage = (name) =>{
 
 const  typeofErrors= {
     "EmptyString":name +' should be a type of text and not empty',
-    "InvalidString" : name + " is not valid,You Should enter valid String",
-    "Required" : name +' is required',
+    "InvalidString" : name + " is not valid,You Should enter a valid " + name,
+    "Required" : name+' is required',
     "InvalidUnit":name+' is not valid unit for this shipment service',
-    "Notnumber" : name +" isnot valid number , You should enter valid number"
+    "Notnumber" : name+" isnot valid number,You should enter valid number",
+    "MinRequired" : name+" can not be less than 1"
 }
 return typeofErrors;
 
